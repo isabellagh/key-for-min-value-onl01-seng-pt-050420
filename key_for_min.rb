92b74f67855f68176719643b566a33d5de8ab2f2
isabella g henriques <isabellagh88@gmail.com>
2020-05-08 19:47:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,8 @@
 def key_for_min_value(name_hash)
   smallest_hash_key = nil
   name_hash.each do |key, val|
-    smallest_hash_key ||= key
+    smallest_hash_key || = key
     smallest_hash_key = key if val < name_hash[smallest_hash_key]
   end
   smallest_hash_key
+end
